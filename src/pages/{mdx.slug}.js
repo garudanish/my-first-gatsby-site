1cece68b62fde3f592877fa8031b11545c57dc46
Hyeonsu Kim <hyeonsu.kim.dev@gmail.com>
2022-03-23 15:45:36 +0900

    Create blog post page template

---
+++ src/pages/{mdx.slug}.js
@@ -0,0 +1,10 @@
+import * as React from "react";
+import Layout from "../components/layout";
+
+const BlogPost = () => (
+  <Layout pageTitle="Blog Posts">
+    <p>My blog post contents will go here(eventually).</p>
+  </Layout>
+);
+
+export default BlogPost;
