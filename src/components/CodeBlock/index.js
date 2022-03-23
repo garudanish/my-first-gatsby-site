56f008486218f96e9baac0044dc654cfd0bfd42f
garudanish <hyeonsu.kim.dev@gmail.com>
2022-03-24 00:49:59 +0900

    Style and lint

--- src/components/CodeBlock/index.js
+++ src/components/CodeBlock/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import Highlight, { defaultProps } from "prism-react-renderer";
-import theme from "../../node_modules/prism-react-renderer/themes/github";
-import { prismCode } from "./CodeBlock.module.css";
+import theme from "../../../node_modules/prism-react-renderer/themes/github";
+import "./style.scss";
 
 const CodeBlock = ({ children }) => {
   // 언어를 찾기위한 과정
@@ -17,7 +17,7 @@ const CodeBlock = ({ children }) => {
       theme={theme}
     >
       {({ className, tokens, getLineProps, getTokenProps }) => (
-        <pre className={(className, prismCode)}>
+        <pre className={(className, "prism-code")}>
           {tokens.map((line, i) => (
             <div {...getLineProps({ line, key: i })}>
               {line.map((token, key) => (
--- src/components/layout/index.js
+++ src/components/layout/index.js
@@ -1,9 +1,10 @@
 import * as React from "react";
 import { useStaticQuery, graphql } from "gatsby";
-import "../styles/normalize.scss";
-import "../styles/reset.scss";
-import GlobalHeader from "./global-header";
-import Footer from "./footer";
+import "../../styles/normalize.scss";
+import "../../styles/reset.scss";
+import "./style.scss";
+import GlobalHeader from "../global-header";
+import Footer from "../footer";
 
 const Layout = ({ pageTitle, children }) => {
   const data = useStaticQuery(graphql`
