bab5b3f5d010e1685cee42f1122a2ac2e7cef053
Hyeonsu Kim <hyeonsu.kim.dev@gmail.com>
2022-03-23 21:20:08 +0900

    Apply webfont

---
+++ gatsby-browser.js
@@ -0,0 +1,6 @@
+import "@fontsource/noto-sans-kr/100.css";
+import "@fontsource/noto-sans-kr/400.css";
+import "@fontsource/noto-sans-kr/700.css";
+import "@fontsource/noto-sans-kr/900.css";
+
+import "@fontsource/nanum-gothic-coding/400.css";
